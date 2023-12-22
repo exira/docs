8cb85181af3e6a04ddd3123f8c95f23b74a26100
David Cumps <david@cumps.be>
2023-12-23 00:35:58 +0100

    feat: add more meta tags

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,7 +69,13 @@ const config: Config = {
     image: 'img/social-card.png',
 
     metadata: [
-      {name: 'keywords', content: 'domain, domainname, domain name, domainnames, domain names'},
+      { name: 'keywords', content: 'domain, domainname, domain name, domainnames, domain names' },
+      { name: 'apple-mobile-web-app-title', content: 'help.exira.com' },
+      { name: 'application-name', content: 'help.exira.com' },
+      { name: 'msapplication-TileColor', content: '#130046' },
+      { name: 'msapplication-TileImage', content: 'https://exira.com/favicons/mstile-144x144.png' },
+      { name: 'msapplication-config', content: 'https://exira.com/favicons/browserconfig.xml' },
+      { name: 'theme-color', content: '#130046' },
     ],
 
     colorMode: {
